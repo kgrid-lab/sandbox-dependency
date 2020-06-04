b8eaa8d04b2e7800cdad08f3c792d53ab4854a05
mradamgbeck <mradamgbeck@gmail.com>
2020-06-04 10:53:29 -0400

    Initial commit

---
+++ src/test/java/com/kgrid/sandboxdependency/SandboxDependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgrid.sandboxdependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxDependencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
