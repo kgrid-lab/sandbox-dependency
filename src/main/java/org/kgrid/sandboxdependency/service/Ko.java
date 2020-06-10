99b337ba01c63e597a1cfdebeb18bad280dc1ba2
Peter Boisvert <pboisver@umich.edu>
2020-06-10 08:55:42 -0400

    Add sample shelf classes for use in sample adapter

---
+++ src/main/java/org/kgrid/sandboxdependency/service/Ko.java
@@ -0,0 +1,23 @@
+package org.kgrid.sandboxdependency.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Ko {
+  static Map<String,String> rules = new HashMap<>();
+
+  static {
+    rules.put("foo","bar");
+    rules.put("apple","banana");
+    rules.put("up","down");
+    rules.put("left","right");
+  }
+
+  public String id() { return UUID.randomUUID().toString(); }
+
+  public String apply(String key) {
+    return rules.get(key);
+  }
+
+}
---
+++ src/main/java/org/kgrid/sandboxdependency/service/Shelf.java
@@ -0,0 +1,9 @@
+package org.kgrid.sandboxdependency.service;
+
+public class Shelf {
+
+  public int findAll() {
+    return 42;
+  }
+
+}
